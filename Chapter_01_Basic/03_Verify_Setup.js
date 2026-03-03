5cb1f9074df5ca6e300ff3f688363146111809f6
ЖйґДЮЩЯФмоПуц <zjgdusafmopuc@admins-MacBook-Pro.local>
2026-03-03 22:07:24 +0530

    New Basic File

---
+++ Chapter_01_Basic/03_Verify_Setup.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+// Mac -- Darwin
+// Windows -- win32
+// Linux -- linux
+
+console.log(process.arch);
+// x64 -- 64-bit
+// ia32 -- 32-bit
+// arm -- ARM architecture  
+
+console.log("Node Version: ", process.version);
---
+++ Chapter_01_Basic/04_hot_code.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+
+function add(a,b){
+    return a + b;   
+}
+
+let result = add(10, 20);
+console.log(result);
